bcc1fd656403b1297e0c693ed42e1d5826db1304
MrDallOca <tdalloca@gmail.com>
2017-06-09 10:55:50 -0300

    Quase tudo pronto

--- LeitorDeTesouros/app/src/main/java/masterplan/leitordetesouros/MainActivity.java
+++ LeitorDeTesouros/app/src/main/java/masterplan/leitordetesouros/MainActivity.java
@@ -6,13 +6,17 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
-import android.provider.CalendarContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -22,7 +26,6 @@ import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
-import org.apache.commons.codec.binary.Base64;
 import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
@@ -32,14 +35,22 @@ import java.util.Map;
 
 import cz.msebera.android.httpclient.Header;
 
+import static android.util.Base64.decode;
+
 public class MainActivity extends AppCompatActivity {
 
     AsyncHttpClient client;
+    private Double latitude;
+    private Double longitude;
+    private LocationManager locationManager;
+    private  Bitmap bmpTemp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bmpTemp = null;
 
         client = new AsyncHttpClient();
         final IntentIntegrator integrator = new IntentIntegrator(this);
@@ -51,6 +62,41 @@ public class MainActivity extends AppCompatActivity {
                 integrator.initiateScan();
             }
         });
+
+        latitude = new Double(0);
+        longitude = new Double(0);
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String bestProvider = String.valueOf(locationManager.getBestProvider(criteria, true)).toString();
+        locationManager.requestLocationUpdates(bestProvider, 1000, 0, locationListener);
+
     }
 
     @Override
@@ -63,49 +109,35 @@ public class MainActivity extends AppCompatActivity {
 
             Calendar c = Calendar.getInstance();
             c.add(Calendar.DATE, 1);
-            SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-DD hh:mm:ss");
+            SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
             String formatted = format1.format(c.getTime());
-
-            LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            Double longitude = location.getLongitude();
-            Double latitude = location.getLatitude();
 
             Map<String, String> params = new HashMap<String, String>();
             params.put("action", "get");
-            params.put("ra", "00001");
+            params.put("ra", "15194");
             params.put("dt", formatted);
             params.put("lat", latitude.toString());
-            params.put("lon", latitude.toString());
+            params.put("lon", longitude.toString());
+            params.put("id", re);
 
             SergioRestClient.post(new RequestParams(params), new AsyncHttpResponseHandler() {
                 @Override
                 public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                     try {
-                        JSONObject serverResp = new JSONObject(responseBody.toString());
+                        String resposta = new String(responseBody);
+                        JSONObject serverResp = new JSONObject(resposta);
                         Toast toast = Toast.makeText(getApplicationContext(), serverResp.getString("dica"), Toast.LENGTH_SHORT);
                         toast.show();
 
-                        ImageView image = (ImageView) findViewById(R.id.imageView);
-
                         String imageDataString = serverResp.getString("image");
-                        byte[] imageDataBytes = Base64.decodeBase64(imageDataString);
+                        byte[] imageDataBytes = decode(imageDataString, Base64.DEFAULT);
 
-                        Bitmap bmp = BitmapFactory.decodeByteArray(imageDataBytes, 0, imageDataBytes.length);
-
-                        image.setImageBitmap(Bitmap.createScaledBitmap(bmp, image.getWidth(), image.getHeight(), false));
+                        bmpTemp = BitmapFactory.decodeByteArray(imageDataBytes, 0, imageDataBytes.length);
+                        updateImage();
                     }
-                    catch (Exception e){}
+                    catch (Exception e){
+                        Log.v("Deu ruim: ", e.getMessage());
+                    }
 
                 }
 
@@ -113,6 +145,32 @@ public class MainActivity extends AppCompatActivity {
                 public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
                 }
             });
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        savedInstanceState.putParcelable("BitmapImage", bmpTemp);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+        bmpTemp = savedInstanceState.getParcelable("BitmapImage");
+        ImageView image = (ImageView) findViewById(R.id.imageView);
+        ViewTreeObserver vto = image.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                updateImage();
+            }
+        });
+    }
+
+    protected void updateImage(){
+        if (bmpTemp != null){
+            ImageView image = (ImageView) findViewById(R.id.imageView);
+            image.setImageBitmap(Bitmap.createScaledBitmap(bmpTemp, image.getWidth(), image.getHeight(), false));
+            image.requestLayout();
         }
     }
 }
